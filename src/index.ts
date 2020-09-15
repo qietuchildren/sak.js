19fd294967b8058eddafac0d9a42afd58c221c0c
Rohy <rohy.froze@gmail.com>
2020-09-15 18:03:37 +0800

    feat: use export instead of import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import CookieUtils from '../lib/cookie-utils/cookie'
-import MathUtils from '../lib/math-utils/math'
-import DateUtils from '../lib/date-utils/date'
-import UrlUtils from '../lib/url-utils/url'
-
-export default {
-  CookieUtils,
-  MathUtils,
-  DateUtils,
-  UrlUtils
-}
+export * from '../lib/cookie-utils/cookie'
+import * from '../lib/math-utils/math'
+import * from '../lib/date-utils/date'
+import * from '../lib/url-utils/url'
